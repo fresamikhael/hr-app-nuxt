37f66a8875f29ae8f1046846d46d2c2ea30c314f
fresaletlora <mikhaelfresa@gmail.com>
2023-02-11 08:18:42 +0700

    first commit

---
+++ layouts/blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div><h1>My blog navigation bar here</h1></div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blog',
+  // OR
+  layout(context) {
+    return 'blog'
+  },
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello World Change</h1>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+  layout: 'blog',
+}
+</script>
