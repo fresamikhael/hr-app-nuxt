6758128777840e1829a661428b800a1a589c7319
fresaletlora <mikhaelfresa@gmail.com>
2023-02-13 22:16:12 +0700

    apdets

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,13 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'IndexPage',
+  async asyncData({ params }) {
+    const post = await axios.get(`https://api.nuxtjs.dev/posts/1`)
+    return { post }
+  },
 }
 </script>
